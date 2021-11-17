d1a1220b316906dc4d61b40eef3bee0441f67c2d
usha25 <mahamudausha25@gmail.com>
2021-11-18 00:35:14 +0600

    New java class added

---
+++ Calculator/app/src/main/java/me/mahakagg/calculator/firefly.java
@@ -0,0 +1,17 @@
+package me.mahakagg.calculator;
+
+public class firefly {
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public firefly(int a) {
+        this.a = a;
+    }
+}
